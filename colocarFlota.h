68e3a5ce0033b80e31f726ac4c38b405bdc8f97e
José Gómez García <joseegg71@gmail.com>
2025-04-03 16:52:29 +0200

    Versión 1.9

--- colocarFlota.h
+++ colocarFlota.h
@@ -9,9 +9,6 @@
 //Precondición: tener inicializados los tableros y recibir el número de barcos para saber las iteraciones
 //Cabecera: (char **) colocarBarcos (jugador* jug, int numBarcos)
 //Postcondición: devuelve una matriz con los barcos colocados
-
 char ** colocarBarcos(jugador *,Barcos *, int);
-
-
 
 #endif // COLOCAR_BARCOS_H
