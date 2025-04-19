74454859c3092d8f307b4aadd970d8db195b4098
Daniel <dangonvega@gmail.com>
2025-04-19 13:02:52 +0200

    Update disparoAutomatico.h

--- disparoAutomatico.h
+++ disparoAutomatico.h
@@ -1,12 +1,14 @@
 #ifndef DISPARO_AUTOMATICO_H_INCLUDED
 #define DISPARO_AUTOMATICO_H_INCLUDED
 
-#include "tablero.h"
+#include "configuracion.h"
 #include "jugador.h"
 #include <stdlib.h>
+#include <time.h>
 
-void disparo_automatico(char tablero_oponente[N][N], int *fila, int *columna, int *ultimo_fila, int *ultimo_columna, int *tocado, int *direccion_fila, int *direccion_columna, int *impactos);
-void disparo_manual(Jugador *jugador, Jugador *oponente,int *fila,int *columa);
+void disparo_automatico(char **tablero_oponente,int tam_tablero, int *fila, int *columna, int *ultimo_fila, int *ultimo_columna, int *tocado, int *direccion_fila, int *direccion_columna, int *impactos);
+void disparo_manual(int *fila,int *columa);
 
 
 #endif // DISPARO_AUTOMATICO_H_INCLUDED
+
