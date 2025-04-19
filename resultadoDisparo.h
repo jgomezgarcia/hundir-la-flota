20a5e8158b3746ae81f75548dc33ea90c03b6351
apmarti06 <apachecomarti@gmail.com>
2025-04-19 13:43:58 +0200

    Update resultadoDisparo.h

--- resultadoDisparo.h
+++ resultadoDisparo.h
@@ -1,1 +1,14 @@
+#ifndef RESULTADOS_H
+#define RESULTADOS_H
+
+
+#include "configuracion.h"
+#include "disparoAutomatico.h"
+#include "interfaces.h"
+
+int comprobar_resultados_disparo(jugador* defensor, int* fila, int* columna, int tam_tablero, int* tocado);
+void gestionar_turnos (jugador *jug1,jugador *jug2, int tam_tablero);
+
+
+#endif // RESULTADOS_H
 
