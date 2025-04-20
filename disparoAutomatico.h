58aab7986396619891914ab25ebbc28a64617833
Daniel <dangonvega@gmail.com>
2025-04-21 00:20:54 +0200

    disparoAutomatico.h

--- disparoAutomatico.h
+++ disparoAutomatico.h
@@ -6,8 +6,19 @@
 #include <stdlib.h>
 #include <time.h>
 
-void disparo_automatico(char **tablero_oponente,int tam_tablero, int *fila, int *columna, int *ultimo_fila, int *ultimo_columna, int *tocado, int *direccion_fila, int *direccion_columna, int *impactos);
 
+void disparo_automatico(
+    char **tablero_oponente,
+    int tam_tablero,
+    int *fila,
+    int *columna,
+    int *ultimo_fila,
+    int *ultimo_columna,
+    int *tocado,
+    int *direccion_fila,
+    int *direccion_columna,
+    int *impactos,
+    int intentos_adicionales[8]  // Array de control para saber qué direcciones ya fueron probadas
+);
 
-#endif // DISPARO_AUTOMATICO_H
-
+#endif
